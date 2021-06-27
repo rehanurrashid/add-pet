3cb48d0b464c2224effc3364ba13e4fe95622a84
Rehan ur Rashid <54805360+rehanurrashid@users.noreply.github.com>
2021-06-27 16:36:38 +0500

    Add files via upload

---
+++ includes/class-ap-ss-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://dev.technology-architects.com/petgolu/
+ * @since      1.0.0
+ *
+ * @package    Ap_Ss
+ * @subpackage Ap_Ss/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Ap_Ss
+ * @subpackage Ap_Ss/includes
+ * @author     Technology Architects
+ */
+class Ap_Ss_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
